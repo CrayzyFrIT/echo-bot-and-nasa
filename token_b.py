1453be694b89318d330a3fad5507ccae97cd0396
CrayzyFrIT <98195691+CrayzyFrIT@users.noreply.github.com>
2022-04-07 12:41:49 +0300

    Update token_b.py

--- token_b.py
+++ token_b.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-TOKEN = '5227083432:AAE7X2lv8x558vA9VPv-IsPwNbNNLsDP6Tg'
-=======
 TOKEN = 'your token'
->>>>>>> e6ef8a927257d42a90786accb85845cf8522cb96
 
 URL_NASA = 'https://images-api.nasa.gov/search?media_type=image&year_start=2022'
